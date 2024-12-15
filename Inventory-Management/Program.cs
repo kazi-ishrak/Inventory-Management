8d784f0bb7a721004a6e7067fb886853bee4ef87
Zafir <55599023+zafir100100@users.noreply.github.com>
2024-12-15 21:57:32 +0600

    startup is broken down to region

--- Inventory-Management/Program.cs
+++ Inventory-Management/Program.cs
@@ -3,8 +3,10 @@ using Inventory_Management.Repositories;
 using Inventory_Management.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
+#region Service Registrations
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 IConfiguration configuration = builder.Configuration;
@@ -19,25 +21,24 @@ builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Central Server API", Version = "v1" });
 });
-
+#endregion
 
 #region DB Connection
-//Local
+// Local
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-
-
     options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), sqlOptions =>
     {
         sqlOptions.EnableRetryOnFailure(maxRetryCount: 3, maxRetryDelay: TimeSpan.FromSeconds(10), errorNumbersToAdd: null);
     });
-
 }, ServiceLifetime.Scoped);
-
 #endregion
 
+#region Build Application
 var app = builder.Build();
+#endregion
 
+#region HTTP Request Pipeline
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -48,16 +49,22 @@ if (!app.Environment.IsDevelopment())
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Inovace Central Server");
     });
 }
+#endregion
 
-
+#region Middleware Configuration
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthorization();
+#endregion
 
+#region Route Configuration
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Products}");
+#endregion
 
+#region Application Execution
 app.Run();
+#endregion
