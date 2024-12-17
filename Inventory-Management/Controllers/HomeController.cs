86a438add0a5e9bbe7f5eda1331853d727ce4cb7
kazi-ishrak <kazi.ishrak83@gmail.com>
2024-12-17 16:19:59 +0600

    audit log ok

--- Inventory-Management/Controllers/HomeController.cs
+++ Inventory-Management/Controllers/HomeController.cs
@@ -29,7 +29,10 @@ namespace Inventory_Management.Controllers
         {
             return View();
         }
-
+        public IActionResult AuditLogs()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
